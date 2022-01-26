f2dea581a6b45f7a4f797e931ec024d3d06dbb7a
Christo <christotantony2003@gmail.com>
2022-01-26 22:59:48 +0400

    fixes

--- index.py
+++ index.py
@@ -2,20 +2,3 @@ from time import sleep
 from CLI.app import runApp
 
 runApp()
-
-# from HandBrake.Command import Command
-# from HandBrake.Media import Media
-# from HandBrake.ProcessHandler import ProcessHandler
-
-# command1 = Command(media=Media("/mnt/media/Movies/English/Dune (2021)/Dune (2021).mp4"))
-# command2 = Command(media=Media("/mnt/media/Movies/English/Dune (2021)/Dune (2021).mp4"))
-# p = ProcessHandler()
-# p.addProcess(command1)
-# p.addProcess(command2)
-# p.run()
-# # print(p.isRunning.value())
-# while True : 
-#     # input()
-#     sleep(1)
-#     # print("Job Queue: ", p.getQueuedProcess() )
-#     if (p.getCurrentProcess()) : print(p.getStats())
